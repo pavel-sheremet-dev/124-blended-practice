7123e1d88506b35cdeeb149b50221b2047c0215c
Vovk <vovkvaleria06@gmail.com>
2026-04-11 15:24:50 +0300

    add postService

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,19 +4,37 @@ import SearchBox from "../SearchBox/SearchBox";
 import Pagination from "../Pagination/Pagination";
 
 import css from "./App.module.css";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { fetchPosts } from "../../services/postService";
+import { useState } from "react";
 
 export default function App() {
+  const [search, setSearch] = useState("");
+  const [page, setPage] = useState(1);
+
+  const { data, isSuccess } = useQuery({
+    queryKey: ["post", page],
+    queryFn: () => fetchPosts(search, page),
+    placeholderData: keepPreviousData,
+  });
+
+  const handlePage = (page: number) => {
+    setPage(page);
+  };
+
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
         <SearchBox />
-        <Pagination currentPage={1} onPageChange={() => {}} totalPages={5} />
+        {isSuccess && (
+          <Pagination currentPage={page} onPageChange={handlePage} totalPages={data.totalPages} />
+        )}
         <button className={css.button}>Create post</button>
       </header>
       <Modal>
         <div>Контент модального вікна</div>
       </Modal>
-      <PostList />
+      {isSuccess && <PostList posts={data.posts} />}
     </div>
   );
 }
--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,8 +1,19 @@
 import axios from "axios";
+import { Post } from "../types/post";
 
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 
-export const fetchPosts = async (searchText, page) => {};
+export const fetchPosts = async (searchText: string, page: number) => {
+  const res = await axios.get<Post[]>("/posts", {
+    params: {
+      q: searchText,
+      _page: page,
+      _limit: 12,
+    },
+  });
+  const totalPages = Math.ceil(res.headers["x-total-count"] / 12);
+  return { posts: res.data, totalPages };
+};
 
 export const createPost = async (newPost) => {};
 
